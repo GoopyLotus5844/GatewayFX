7f0de1a9802869cbe5e7dba6cdce644feb9ae30c
Hank Stennes <hpstennes@gmail.com>
2021-11-23 21:11:26 -0600

    Make draggable properties window over canvas

--- src/main/java/com/gateway/main/HelloController.java
+++ src/main/java/com/gateway/main/HelloController.java
@@ -1,19 +1,42 @@
 package com.gateway.main;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    private ToggleButton myButton;
+    public Canvas canvas;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public DraggableNode paneProperties;
+
+    @FXML
+    private ToggleButton buttonDelete;
+
+    @FXML
+    private Pane paneCenter;
+
+    @FXML
+    public void initialize(){
+        System.out.println("Absolutely froggers");
+        canvas.widthProperty().bind(paneCenter.widthProperty());
+        canvas.heightProperty().bind(paneCenter.heightProperty());
+    }
+
+    @FXML
+    protected void onDeleteButton(){
+        System.out.println("Here");
+        GraphicsContext g = canvas.getGraphicsContext2D();
+
+        if(buttonDelete.isSelected()) g.setFill(Color.BLUE);
+        else g.setFill(Color.RED);
+
+        System.out.println(canvas.getWidth());
+        g.fillOval(0, 0, 50, 50);
     }
 }
